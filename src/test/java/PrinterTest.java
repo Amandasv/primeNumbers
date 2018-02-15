cb75a6f550604065f2b54a753560137567960a50
amandasv <amandasv.pm@gmail.com>
2018-02-15 10:44:22 -0200

    Add printer class

---
+++ src/test/java/PrinterTest.java
@@ -0,0 +1,34 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.Assert.assertEquals;
+
+
+public class PrinterTest {
+
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+
+    @Before
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+    }
+
+    @After
+    public void restoreStreams() {
+        System.setOut(System.out);
+    }
+
+    @Test
+    public void printsMessagePassed(){
+        Printer printer = new Printer();
+        String words = "Printer Test";
+
+        printer.print(words);
+
+        assertEquals("Printer Test", outContent.toString());
+    }
+}
